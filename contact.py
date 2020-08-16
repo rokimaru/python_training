f24d433df9328f749182e489638763f708df2e32
rokimaru <rokimaru@users.noreply.github.com>
2020-08-16 18:07:49 +0300

    добавлен класс contact.py

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self, name, middle_name, surname, nickname, company, title, telephone_home,
+                          telephone_mobile, telephone_work, fax, email1, email2, email3, homepage, bday, bmonth, byear,
+                          aday, amonth, ayear, address, home, note):
+        self.name = name
+        self.middle_name = middle_name
+        self.surname = surname
+        self.nickname = nickname
+        self.company = nickname
+        self.title = title
+        self.telephone_home = telephone_home
+        self.telephone_mobile = telephone_mobile
+        self.telephone_work = telephone_work
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address = address
+        self.home = home
+        self.note = note
+
