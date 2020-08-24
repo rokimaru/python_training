51e6776ad5a39bde712d2d58a5717e167c1d5d50
rokimaru <asya3.14159@gmail.com>
2020-08-24 15:05:40 +0300

    вспомогательные методы из модуля test_add_group перенесены в отдельный класс Application

--- test_add_group.py
+++ test_add_group.py
@@ -7,7 +7,6 @@ import unittest
 from application import Application
 from group import Group
 
-
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.app = Application()
@@ -21,6 +20,21 @@ class TestAddGroup(unittest.TestCase):
         self.login(username="admin", password="secret")
         self.create_group(Group(name="", header="", footer=""))
         self.logout()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
 
     def tearDown(self):
         self.wd.quit()
