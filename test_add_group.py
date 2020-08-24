bb4a17285803d48c0b8baff8f44c15cb4e59eecd
rokimaru <asya3.14159@gmail.com>
2020-08-24 13:08:39 +0300

    Возврат на предыдущую сраницу вынесен в метод создания группы

--- test_add_group.py
+++ test_add_group.py
@@ -15,18 +15,13 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="test", header="test", footer="test"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -37,6 +32,7 @@ class TestAddGroup(unittest.TestCase):
 
     def create_group(self, wd, group):
         # init group creation
+        self.open_groups_page(wd)
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
@@ -50,6 +46,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
