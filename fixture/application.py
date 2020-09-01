5765f0cf89ee643040c72652e5110f583ee0263a
rokimaru <rokimaru@users.noreply.github.com>
2020-09-01 12:04:02 +0300

    переходы на главную страницу оптимизированы и перенесены во вспомогательный класс ContactHelper

--- fixture/application.py
+++ fixture/application.py
@@ -14,20 +14,10 @@ class Application:
 
     def is_valid(self):
         try:
-             self.wd.current_url
+             self.wd.current_url()
              return True
         except:
             return False
-
-    def open_home_page(self):
-        wd = self.wd
-        if not (wd.current_url == "http://localhost/addressbook/"):
-            wd.get("http://localhost/addressbook/")
-
-    def return_to_home_page(self):
-        wd = self.wd
-        if not (wd.current_url == "http://localhost/addressbook/"):
-            wd.find_element_by_link_text("home page").click()
 
     def destroy(self):
         self.wd.quit()
