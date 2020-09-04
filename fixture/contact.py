3478397cc9f042068bba7eb9e6297d782a7537ac
rokimaru <rokimaru@users.noreply.github.com>
2020-09-04 15:44:47 +0300

    унифицирован метод редактирования полей контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -13,7 +13,6 @@ class ContactHelper:
         wd.find_element_by_link_text("add new").click()
 
     def edit_fields(self, contact):
-        wd = self.app.wd
         self.change_field_value("firstname", contact.name)
         self.change_field_value("middlename", contact.middle_name)
         self.change_field_value("lastname", contact.surname)
@@ -30,31 +29,12 @@ class ContactHelper:
         self.change_field_value("email2", contact.email2)
         self.change_field_value("email3", contact.email3)
         self.change_field_value("homepage", contact.homepage)
-        # # birthday
-        # wd.find_element_by_name("bday").click()
-        # Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        # wd.find_element_by_name("bday").click()
-        # wd.find_element_by_name("bmonth").click()
-        # Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        # wd.find_element_by_name("bmonth").click()
-        # wd.find_element_by_name("byear").click()
-        # wd.find_element_by_name("byear").clear()
-        # wd.find_element_by_name("byear").send_keys(contact.byear)
-        # # anniversary
-        # wd.find_element_by_name("aday").click()
-        # Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        # wd.find_element_by_name("aday").click()
-        # wd.find_element_by_name("amonth").click()
-        # Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        # wd.find_element_by_name("amonth").click()
-        # wd.find_element_by_name("ayear").click()
-        # wd.find_element_by_name("ayear").clear()
-        self.change_date_value("bday", contact.bday)
-        self.change_date_value("bmonth", contact.bmonth)
-        self.change_date_value("byear", contact.byear)
-        self.change_date_value("aday", contact.aday)
-        self.change_date_value("amonth", contact.amonth)
-        self.change_date_value("ayear", contact.ayear)
+        self.change_field_value("bday", contact.bday)
+        self.change_field_value("bmonth", contact.bmonth)
+        self.change_field_value("byear", contact.byear)
+        self.change_field_value("aday", contact.aday)
+        self.change_field_value("amonth", contact.amonth)
+        self.change_field_value("ayear", contact.ayear)
         self.change_field_value("home", contact.home)
         self.change_field_value("notes", contact.note)
 
@@ -63,19 +43,17 @@ class ContactHelper:
         wd.find_element_by_name("photo").clear()
         wd.find_element_by_name("photo").send_keys("C:\photo.jpg")
 
-    def change_date_value(self, field_name, value):
-        wd = self.app.wd
-        if value is not None:
-            wd.find_element_by_name(field_name).click()
-            Select(wd.find_element_by_name(field_name)).select_by_visible_text(value)
-            wd.find_element_by_name(field_name).click()
-
     def change_field_value(self, field_name, text):
         wd = self.app.wd
         if text is not None:
-            wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
-            wd.find_element_by_name(field_name).send_keys(text)
+            if field_name in ["bday", "bmonth", "byear", "aday", "amonth", "ayear"]:
+                wd.find_element_by_name(field_name).click()
+                Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
+                wd.find_element_by_name(field_name).click()
+            else:
+                wd.find_element_by_name(field_name).click()
+                wd.find_element_by_name(field_name).clear()
+                wd.find_element_by_name(field_name).send_keys(text)
 
     def submit_creation(self):
         wd = self.app.wd
@@ -105,12 +83,22 @@ class ContactHelper:
 
     def count(self):
         wd = self.app.wd
-        self.app.open_home_page()
+        self.open_home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
     def del_first_contact(self):
         wd = self.app.wd
-        self.app.open_home_page()
+        self.open_home_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+
+    def open_home_page(self):
+        wd = self.app.wd
+        if not (wd.current_url == "http://localhost/addressbook/"):
+            wd.get("http://localhost/addressbook/")
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        if not (wd.current_url == "http://localhost/addressbook/"):
+            wd.find_element_by_link_text("home page").click()
