e3a7469114bf60c983797e0f30183cac0b5aad1c
rokimaru <rokimaru@users.noreply.github.com>
2020-09-07 17:20:48 +0300

    проверка на равенство и сортировка добавлена в метод редактирования группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,20 +2,27 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="new name"))
+    group = Group(name="test")
+    group.id = old_groups[0].id
+    if app.group.count() == 0:
+        app.group.create(group)
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_edit_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header="test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(header="new header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_edit_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(header="test")
+#     if app.group.count() == 0:
+#         app.group.create(group)
+#     app.group.edit_first_group(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#     old_groups[0] = group
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
