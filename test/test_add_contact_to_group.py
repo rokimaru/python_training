3a783dc7f1b33ce1d63db135e0c8c2de865aade7
rokimaru <rokimaru@users.noreply.github.com>
2020-10-08 17:30:23 +0300

    правки после ревью

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,23 +1,35 @@
 import random
 from model.contact import Contact
 from fixture.orm import ORMFixture
+from model.group import Group
 
 orm = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 
 def test_add_contact_to_group(app, db):
-    random_contact = random.choice(db.get_contact_list())
-    random_group = random.choice(db.get_group_list())
-    old_contacts_in_group = orm.get_contacts_in_group(random_group)
+    groups = db.get_group_list()
+    contacts = db.get_contacts_list()
+    if len(groups) == 0:
+        app.group.create(Group(name='group_for_adding_contact_test'))
+    if len(contacts) == 0:
+        app.contact.create_contact(Contact(firstname='contact_for_adding_contact_to_group_test'))
 
-    if random_contact not in old_contacts_in_group:
+    random_group = random.choice(groups)
+    old_contacts_in_group = orm.get_contacts_in_group(random_group)
+    contacts_not_in_group = []
+    for contact in contacts:
+        if contact not in old_contacts_in_group:
+            contacts_not_in_group.append(contact)
+    if len(contacts_not_in_group) != 0:
+        random_contact = random.choice(contacts_not_in_group)
         app.contact.add_contact_by_id_in_group(random_contact, random_group)
+        old_contacts_in_group.append(random_contact)
     else:
-        app.contact.del_contact_by_id_from_group(random_contact, random_group)
-        old_contacts_in_group = orm.get_contacts_in_group(random_group)
-        app.contact.add_contact_by_id_in_group(random_contact, random_group)
+        new_contact = app.contact.create_contact(Contact(firstname='contact_for_adding_contact_to_group_test'))
+        app.contact.add_contact_by_id_in_group(new_contact, random_group)
+        old_contacts_in_group.append(new_contact)
 
     new_contacts_in_group = orm.get_contacts_in_group(random_group)
-    old_contacts_in_group.append(random_contact)
     assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
+
 
--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -1,19 +1,27 @@
 import random
 from fixture.orm import ORMFixture
 from model.contact import Contact
+from model.group import Group
 
 orm = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 
 def test_del_contact_in_group(app, db):
-    random_contact = random.choice(db.get_contact_list())
-    random_group = random.choice(db.get_group_list())
+    groups = db.get_group_list()
+    contacts = db.get_contacts_list()
+    if len(groups) == 0:
+        app.group.create(Group(name='group_for_adding_contact_test'))
+    if len(contacts) == 0:
+        app.contact.create_contact(Contact(firstname='contact_for_adding_contact_to_group_test'))
+
+    random_group = random.choice(groups)
     old_contacts_in_group = orm.get_contacts_in_group(random_group)
 
-    if random_contact in old_contacts_in_group:
+    if len(old_contacts_in_group) != 0:
+        random_contact = random.choice(old_contacts_in_group)
         app.contact.del_contact_by_id_from_group(random_contact, random_group)
-
     else:
+        random_contact = random.choice(contacts)
         app.contact.add_contact_by_id_in_group(random_contact, random_group)
         old_contacts_in_group = orm.get_contacts_in_group(random_group)
         app.contact.del_contact_by_id_from_group(random_contact, random_group)
@@ -21,3 +29,5 @@ def test_del_contact_in_group(app, db):
     new_contacts_in_group = orm.get_contacts_in_group(random_group)
     old_contacts_in_group.remove(random_contact)
     assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
+
+
