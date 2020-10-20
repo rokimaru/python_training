88b3713fc9e90d727c8d172af5a37af8d1b71b9a
rokimaru <rokimaru@users.noreply.github.com>
2020-10-20 13:47:09 +0300

    Тест на удаление контакта переделать для отчетов allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
 
-        def clean(group):
-            while "  " in group.name:
-                group.name = group.name.replace("  ", " ")
-            return Group(id=group.id, name=group.name.strip())
+            def clean(group):
+                while "  " in group.name:
+                    group.name = group.name.replace("  ", " ")
+                return Group(id=group.id, name=group.name.strip())
 
-        db_list = map(clean, db.get_group_list())
-        assert sorted(db_list, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+            db_list = map(clean, db.get_group_list())
+            assert sorted(db_list, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
+#--junitxml=PATH
+# --alluredir allure-results
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,16 +1,23 @@
 import random
+
+import pytest
+
 from model.contact import Contact
 
 
 def test_delete_some_contact(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.contact.create(Contact(firstname="del_test"))
-    old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
-    app.contact.del_contact_by_id(contact.id)
-    new_contacts = db.get_contact_list()
-    old_contacts.remove(contact)
-    assert old_contacts == new_contacts
+    with pytest.allure.step('Get contact list'):
+        old_contacts = db.get_contact_list()
+    with pytest.allure.step('Choice random contact in contact list'):
+        contact = random.choice(old_contacts)
+    with pytest.allure.step('Delete contact from addressbook'):
+        app.contact.del_contact_by_id(contact.id)
+    with pytest.allure.step('Get new contact list and compare to old contact list without removed contact'):
+        new_contacts = db.get_contact_list()
+        old_contacts.remove(contact)
+        assert old_contacts == new_contacts
     if check_ui:
         assert sorted(db.get_contact_list(), key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                               key=Contact.id_or_max)
